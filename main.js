c5ee03b38c32b199ff6196bcbec4a934f6d5bdda
Marco Agrippa Dev <ma.agippa@gmail.com>
2023-05-04 11:02:08 -0300

    front finalizado html css js

---
+++ main.js
@@ -0,0 +1,14 @@
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keydown", function (event) {
+    if (event.code === "Enter") {
+        search();
+    }
+});
+
+function search() {
+    const input = searchInput.value;
+
+    window.location.href = "https://duckduckgo.com/";
+}
+
